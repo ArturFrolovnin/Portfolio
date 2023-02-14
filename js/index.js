f422c3006f30a109fa4f37aed6de85291a3c5be0
Artur Frolovnin <96828393+Artur78r@users.noreply.github.com>
2023-02-15 00:45:30 +0300

    update

---
+++ js/index.js
@@ -0,0 +1,17 @@
+
+document.getElementById('IdLinkVk').addEventListener('click', function LinkVk() {
+
+    window.location.href ='https://vk.com/arturvladimirovichh';
+    
+});
+
+document.getElementById('IdLinkTelegram').addEventListener('click', function LinkTelegram() {
+
+    window.location.href ='https://t.me/faunra';
+    
+});
+
+
+document.getElementById('IdLinkGithub').addEventListener('click', function LinkGithub() {
+    window.location.href ='https://github.com/Artur78r';
+});
