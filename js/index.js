92189617fefe38711aaaf2807cea421989151d40
Artur Frolovnin <96828393+Artur78r@users.noreply.github.com>
2023-02-17 21:54:52 +0300

    update

--- js/index.js
+++ js/index.js
@@ -1,48 +1,70 @@
 
 document.getElementById('IdLinkVk').addEventListener('click', function LinkVk() {
 
-    window.location.href ='https://vk.com/arturvladimirovichh';
-    
+    window.location.href ='https://vk.com/arturvladimirovichh';   
 });
 
 document.getElementById('IdLinkTelegram').addEventListener('click', function LinkTelegram() {
 
     window.location.href ='https://t.me/faunra';
-    
 });
 
 
 document.getElementById('IdLinkGithub').addEventListener('click', function LinkGithub() {
-    window.location.href ='https://github.com/Artur78r';
 
+    window.location.href ='https://github.com/Artur78r';
 });
+
+
 
 
 
 document.querySelector('.footer-button').addEventListener('click', function LinkTelegram() {
 
     window.location.href ='https://t.me/faunra';
-    
 });
 
 
 
 document.getElementById('buttonNext').onclick = function(){
  
-
 window.scrollTo({top:950, left:0 , behavior: 'smooth'});
-
-
 }
 
 
+document.getElementById('button-target').onclick = function(){
+ 
+window.scrollTo({top:950, left:0 , behavior: 'smooth'});
+    
+}
 
 
+document.getElementById('button-portfolio').onclick = function(){
+ 
+    window.scrollTo({top:1950, left:0 , behavior: 'smooth'});
+        
+}
+    
+    
+
+document.getElementById('button-technologies').onclick = function(){
+ 
+    window.scrollTo({top:3800, left:0 , behavior: 'smooth'});
+            
+}
+
+document.getElementById('button-about').onclick = function(){
+ 
+    window.scrollTo({top:4600, left:0 , behavior: 'smooth'});
+            
+}
 
 
-
-
-
+document.getElementById('button-contacts').onclick = function(){
+ 
+    window.scrollTo({top:5500, left:0 , behavior: 'smooth'});
+            
+}
 
     
         
