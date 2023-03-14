285f76d64423326c99a0f01aba3464c30cb448bd
Artur Frolovnin <96828393+Artur78r@users.noreply.github.com>
2023-03-14 18:40:13 +0300

    update

--- js/index.js
+++ js/index.js
@@ -1,93 +1,45 @@
-
-document.getElementById('IdLinkVk').addEventListener('click', function LinkVk() {
-
-    window.location.href ='https://vk.com/arturvladimirovichh';   
-});
-
-document.getElementById('IdLinkTelegram').addEventListener('click', function LinkTelegram() {
-
-    window.location.href ='https://t.me/faunra';
-});
-
-
-document.getElementById('IdLinkGithub').addEventListener('click', function LinkGithub() {
-
-    window.location.href ='https://github.com/Artur78r';
-});
-
-
-
-
-
-document.querySelector('.footer-button').addEventListener('click', function LinkTelegram() {
-
-    window.location.href ='https://t.me/faunra';
-});
-
-
-
-document.getElementById('buttonNext').onclick = function(){
- 
-const el = document.getElementById('Id-My-activity-title');
-el.scrollIntoView({block: "start", inline: "center", behavior: "smooth"});
-
-
-}
-
-
-
-
-document.getElementById('button-target').onclick = function(){
- 
-    const el = document.getElementById('Id-My-activity-title');
-    el.scrollIntoView({block: "start", inline: "center", behavior: "smooth"});
+document.getElementById('IdLinkVk').onclick = function () {
+    window.location.href = 'https://vk.com/arturvladimirovichh';
     
 }
 
-
-document.getElementById('button-portfolio').onclick = function(){
- 
-    const el = document.getElementById('Id-My-work-scroll');
-    el.scrollIntoView({block: "start", inline: "center", behavior: "smooth"});
-        
-}
-    
-    
-
-document.getElementById('button-technologies').onclick = function(){
- 
-    const el = document.getElementById('Scroll-technologies');
-    el.scrollIntoView({block: "center", inline: "center", behavior: "smooth"});
-   
-            
+document.getElementById('IdLinkTelegram').onclick =  function () {
+    window.location.href = 'https://t.me/faunra';
 }
 
-
-
-
-document.getElementById('button-about').onclick = function(){
- 
-    const el = document.getElementById('Id-im-person-scroll');
-    el.scrollIntoView({block: "start", inline: "center", behavior: "smooth"});
-            
+document.getElementById('IdLinkGithub').onclick = function () {
+    window.location.href = 'https://github.com/Artur78r';
 }
 
-
-document.getElementById('button-contacts').onclick = function(){
- 
-    window.scrollTo({top:5500, left:0 , behavior: 'smooth'});
-            
+document.querySelector('.footer-button').onclick = function () {
+    window.location.href = 'https://t.me/faunra';
 }
 
-    
-        
-      
-   
-    
+document.getElementById('button-title__button-next').onclick = function () {
+    const element = document.getElementById('Id-My-activity-title');
+    element.scrollIntoView({ block: "start", inline: "center", behavior: "smooth" });
+}
 
+document.getElementById('button-target').onclick = function () {
+    const element = document.getElementById('Id-My-activity-title');
+    element.scrollIntoView({ block: "start", inline: "center", behavior: "smooth" });
+}
 
+document.getElementById('button-portfolio').onclick = function () {
+    const element = document.getElementById('Id-My-work-scroll');
+    element.scrollIntoView({ block: "start", inline: "center", behavior: "smooth" });
+}
 
+document.getElementById('button-technologies').onclick = function () {
+    const element = document.getElementById('Scroll-technologies');
+    element.scrollIntoView({ block: "center", inline: "center", behavior: "smooth" });
+}
 
+document.getElementById('button-about').onclick = function () {
+    const element = document.getElementById('Id-im-person-scroll');
+    element.scrollIntoView({ block: "start", inline: "center", behavior: "smooth" });
+}
 
-
-
+document.getElementById('button-contacts').onclick = function () {
+    window.scrollTo({ top: 5500, left: 0, behavior: 'smooth' });
+}
